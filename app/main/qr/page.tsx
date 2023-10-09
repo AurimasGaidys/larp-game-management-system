7d2359a8c2ac84deb9ea559093986e3bd19f01b3
Aurimas Gaidys <aurimas.gaidys@gmail.com>
2023-10-09 20:43:43 +0200

    Update.

--- app/main/qr/page.tsx
+++ app/main/qr/page.tsx
@@ -42,7 +42,6 @@ export default function Page() {
       }}
       facingMode={"environment"}
       constraints={{
-        audio: true,
         video: { facingMode: "environment" }
       }}
       onError={handleError}
