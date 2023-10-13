86585e82aba1ae66d461206be6ebcefbb39b0419
Aurimas Gaidys <aurimas.gaidys@gmail.com>
2023-10-13 12:48:23 +0200

    Added markdown styles

--- components/dt/elements/text.tsx
+++ components/dt/elements/text.tsx
@@ -1,13 +1,17 @@
 import Markdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 
+import style from './markdown.module.css';
 interface TextProps {
     payload: string
 }
 
 export const TextElement = (p: TextProps) => {
     const textData = JSON.parse(p.payload);
-    return <Markdown remarkPlugins={[remarkGfm]}>
+    return <Markdown
+        remarkPlugins={[remarkGfm]}
+        className={style.reactMarkDown}
+    >
         {textData.texts[0]}
     </Markdown>
 }
