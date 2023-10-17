929ae563f80dc3d3bbc14d8e6eb5ebe6283539a3
Aurimas Gaidys <aurimas.gaidys@gmail.com>
2023-10-17 16:54:19 +0200

    disable buttons.

--- components/base/button/DTButton.tsx
+++ components/base/button/DTButton.tsx
@@ -2,10 +2,11 @@ import { MouseEventHandler } from "react";
 
 interface Props {
     title: string;
+    loading?: boolean;
     onClick: MouseEventHandler<HTMLButtonElement> | undefined
 
 }
 
-export const DTButton = ({ title, onClick }: Props) => {
-    return <button onClick={onClick} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">{title}</button>
+export const DTButton = ({ title, loading, onClick }: Props) => {
+    return <button disabled={loading} onClick={onClick} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">{title}</button>
 }
--- components/dt/elements/button.tsx
+++ components/dt/elements/button.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { onAction } from "../../../dataLayer/apiService";
 import { DTButton } from "../../base/button/DTButton";
 import { ButtonPayload } from "./payloads/dialogElementPayload";
@@ -12,16 +13,20 @@ interface TextProps {
 
 export const ButtonElement = (p: TextProps) => {
     const router = useRouter();
+    const [loading, setLoading] = useState(false);
 
     const cata = JSON.parse(p.payload) as ButtonPayload;
     return <DTButton
+        loading={loading}
         title={cata.name}
         onClick={async () => {
+            setLoading(true);
             const result = await onAction(cata.actionId, p.treeId, "");
             if (result.success) {
                 const url = JSON.parse(result.data).data.url;
-                router.push(url+ `?reload=${new Date().getTime()}`) ;
+                router.push(url + `?reload=${new Date().getTime()}`);
             }
+            setLoading(false);
         }}
     />
 }
