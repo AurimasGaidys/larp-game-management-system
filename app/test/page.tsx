50c492468c3a4fed2d517125ef426fecc1516fea
Aurimas Gaidys <aurimas.gaidys@gmail.com>
2023-12-04 01:05:05 +0100

    Updates tabs.

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -18,6 +18,35 @@ export default function Login() {
     );
   };
 
+  interface ITab {
+    image: string;
+    selected: boolean;
+  }
+
+  const Tab = ({ image, selected }: ITab) => {
+    if (selected) {
+      return (
+        <div className="w-16 h-[66px]  border-[2px] border-[#182130] z-50 m-1 ">
+          <div
+            className={`h-[66px] bg-[#A45078]  border-t-[1px] border-[#D9D9D9] flex flex-row justify-center items-center`}
+          >
+            <div className="w-[2rem] h-[2rem] bg-[url('/bottom-menu/game_rules_icon2.png')] "></div>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="w-16 h-[51px] border-[2px] border-[#182130] z-50 m-1 mt-[14px]">
+          <div
+            className={`h-[51px] bg-[#A45078]  border-t-[1px] border-[#D9D9D9] flex flex-row justify-center items-center`}
+          >
+            <div className="w-[2rem] h-[2rem] bg-[url('/bottom-menu/game_rules_icon2.png')] "></div>
+          </div>
+        </div>
+      );
+    }
+  };
+
   return (
     <div className="flex h-screen bg-gray-100 dark:bg-gray-900 bg-[url('/main/bg.jpg')]">
       <div className="m-auto">
@@ -26,13 +55,13 @@ export default function Login() {
             Dvidlis
           </h1>
 
-          <div className="fixed z-50 w-full h-16 max-w-lg -translate-x-1/2 rounded-full bottom-[8px] left-1/2">
+          <div className="fixed z-50 w-full h-16 max-w-lg -translate-x-1/2 rounded-full bottom-[11px] left-1/2">
             <div className="flex flex-row justify-center">
-              <div className="flex w-[4rem] w-[4rem]  bg-[url('/Tab6.png')] justify-center item-center">
-                <div className="w-[2rem] h-[2rem] bg-[url('/bottom-menu/game_rules_icon2.png')] "></div>
+              <div className="w-[4rem] w-[4rem]  bg-[url('/Tab6.png')]">
+                <div className="w-[4rem] h-[4rem] bg-[url('/bottom-menu/game_rules_icon.png')] "></div>
               </div>
               <div className="flex w-[4rem] w-[4rem] bg-[url('/Tab6.png')] mt-4 justify-center">
-                <div className="w-[2rem] h-[2rem] bg-[url('/bottom-menu/game_rules_icon2.png')] "></div>
+                <div className="w-[4rem] h-[4rem] bg-[url('/bottom-menu/game_rules_icon.png')] "></div>
               </div>
               <div className="w-[4rem] w-[4rem]  bg-[url('/Tab6.png')] mt-4">
                 <div className="w-[3rem] h-[3rem] bg-[url('/bottom-menu/game_rules_icon.png')] "></div>
@@ -40,14 +69,14 @@ export default function Login() {
               <div className="w-16 h-16 bg-[url('/Tab6.png')] mt-4">
                 <div className="w-[3rem] h-[3rem] bg-[url('/bottom-menu/game_rules_icon.png')] "></div>
               </div>
-              <div className="w-16 h-16 bg-[url('/Tab6.png')] mt-4">
-                <div className="w-[3rem] h-[3rem] bg-[url('/bottom-menu/game_rules_icon.png')] mb-4"></div>
-              </div>
-              <div className="w-16 h-16 border-[2px] border-[#182130] z-50">
-                <div className="h-16 bg-[#A45078]  border-t-[1px] border-[#D9D9D9] flex flex-row justify-center items-center">
-                  <div className="w-[2rem] h-[2rem] bg-[url('/bottom-menu/game_rules_icon2.png')] "></div>
-                </div>
-              </div>
+              <Tab
+                image={"/bottom-menu/game_rules_icon2.png"}
+                selected={true}
+              />
+              <Tab
+                image={"/bottom-menu/game_rules_icon2.png"}
+                selected={false}
+              />
             </div>
           </div>
         </div>
@@ -58,3 +87,106 @@ export default function Login() {
     </div>
   );
 }
+
+// export default function Login() {
+//   const createImage = (name: string) => {
+//     return (
+//       <img
+//         src="./assets/image/table_0.png"
+//         id="a118d89a"
+//         draggable="false"
+//         style={{
+//           opacity: 1,
+//           overflow: "visible",
+//           left: 0,
+//           top: 0,
+//           width: 2333.33,
+//           height: 412.533,
+//           transform: "translate(-326.667px, 0px)",
+//         }}
+//       ></img>
+//     );
+//   };
+
+//   interface ITab {
+//     index: number;
+//     image: string;
+//     selected: boolean;
+//   }
+
+//   const Tab = ({ image, selected }: ITab) => {
+//     return (
+//       <div
+//         className={`w-16 h-[${
+//           selected ? "66px" : "51px"
+//         }] border-[2px] border-[#182130] z-50 m-1 ${!selected && "mt-[15px]"}"}`}
+//       >
+//         <div
+//           className={`h-[${
+//             selected ? "66px" : "51px"
+//           }] bg-[#A45078]  border-t-[1px] border-[#D9D9D9] flex flex-row justify-center items-center`}
+//         >
+//           <div className={`w-[20px] h-[20px] bg-[url('/bottom-menu/game_rules_icon2.png')]`}></div>
+//         </div>
+//       </div>
+//     );
+//   };
+
+//   const tabsData: ITab[] = [
+//     {
+//       index: 0,
+//       image: "/bottom-menu/game_rules_icon2.png",
+//       selected: true,
+//     },
+//     {
+//       index: 1,
+//       image: "/bottom-menu/game_rules_icon2.png",
+//       selected: false,
+//     },
+//     {
+//       index: 2,
+//       image: "/bottom-menu/game_rules_icon2.png",
+//       selected: false,
+//     },
+//     {
+//       index: 3,
+//       image: "/bottom-menu/game_rules_icon2.png",
+//       selected: false,
+//     },
+//     {
+//       index: 4,
+//       image: "/bottom-menu/game_rules_icon2.png",
+//       selected: false,
+//     },
+//     {
+//       index: 5,
+//       image: "/bottom-menu/2.png",
+//       selected: false,
+//     },
+//   ];
+
+//   return (
+//     <div className="flex h-screen bg-gray-100 dark:bg-gray-900 bg-[url('/main/bg.jpg')]">
+//       <div className="m-auto">
+//         <div>
+//           <h1 className="text-6xl uppercase font-extrabold text-center">
+//             Dvidlis
+//           </h1>
+
+//           <div className="fixed z-50 w-full h-[66px] max-w-lg -translate-x-1/2 rounded-full bottom-[11px] left-1/2">
+//             <div className="flex flex-row justify-center">
+//               {tabsData.map((x) => {
+//                 return (
+//                   <Tab index={x.index} image={x.image} selected={x.selected} />
+//                 );
+//               })}
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//       <div className="fixed z-0 h-[10px] -bottom-px w-full border-[2px] border-[#182130] z-40">
+//         <div className="h-[10px] bg-[#A45078]  border-t-[1px] border-[#D9D9D9]"></div>
+//       </div>
+//     </div>
+//   );
+// }
