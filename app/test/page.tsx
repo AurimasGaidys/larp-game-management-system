8211500493557f50cb43c132e1c16788642dfbec
Aurimas Gaidys <aurimas.gaidys@gmail.com>
2023-12-06 01:33:42 +0100

    List of lead, custom fonts

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,3 +1,10 @@
+import { Kalam } from "next/font/google";
+
+const roboto = Kalam({
+  weight: "300",
+  subsets: ["latin"],
+});
+
 export default function Login() {
   const createImage = (name: string) => {
     return (
@@ -48,16 +55,16 @@ export default function Login() {
   };
 
   return (
-    <div className="position: relative flex h-screen bg-gray-100 dark:bg-gray-900 bg-[url('/main/bg.jpg')] bg-cover">
+    <div className="relative flex bg-gray-100 dark:bg-gray-900 bg-[url('/main/bg.jpg')] bg-repeat">
       <div className="absolute top-[20px] left-[200px] bgh-center bg-no-repeat h-[249px] w-[249px] bg-[url('/main/magnifying_glass.png')] bg-contain z-50"></div>
       <div className="m-auto">
         <div>
           <div className="absolute top-0 left-0 bg-right-top bg-no-repeat h-full w-full bg-[url('/main/envelope.png')] bg-[length:487px_259px]">
             <div className="h-full w-full max-w-lg flex flex-col">
               <div className="h-[80px]"></div>
-            <div className="bg-no-repeat h-[26px] w-[234px] bg-[url('/main/leads.png')] bg-contain flex items-center justify-center"></div>
+              <div className="bg-no-repeat h-[26px] w-[234px] bg-[url('/main/leads.png')] bg-contain flex items-center justify-center"></div>
               <div className="bg-no-repeat h-[220px] w-[234px] bg-[url('/main/sticky_note.png')] bg-contain flex items-center justify-center">
-                <p className="w-[170px] h-[170px]">
+                <p className={`w-[170px] h-[170px] ${roboto.className}`}>
                   Tekstas tekstas tekstas Tekstas tekstas tekstas Tekstas
                   tekstas tekstas
                 </p>
@@ -65,9 +72,79 @@ export default function Login() {
             </div>
           </div>
 
-          <h1 className="text-6xl uppercase font-extrabold text-center">
-            Dvidlis
-          </h1>
+          <div className="h-[350px]" />
+          <div className="relative h-[150px] w-[375px] bg-no-repeat bg-[url('/main/ticket_bg.png')] bg-contain flex p-2">
+            <div className={`w-[100px] h-[150px] flex flex-col items-start justify-center`}>
+              <p className={`w-[100px] text-[#A81600] ${roboto.className}`}>Lead number one</p>
+              <p
+                className={`flex items-center justify-center w-[80px] h-[36px] bg-no-repeat bg-contain text-[#A81600] text-center bg-[url('/main/read_more_bg.png')] ${roboto.className}`}
+              >
+                Read more
+              </p>
+            </div>
+            <div className="relative h-[136px] w-[236px] bg-no-repeat bg-[url('/main/ticket_thumb.png')] bg-contain"></div>
+          </div>
+
+          <div className="relative h-[150px] w-[375px] bg-no-repeat bg-[url('/main/ticket_bg.png')] bg-contain flex p-2">
+            <div className={`w-[100px] h-[150px] flex flex-col items-start justify-center`}>
+              <p className={`w-[100px] text-[#A81600] ${roboto.className}`}>Lead number one</p>
+              <p
+                className={`flex items-center justify-center w-[80px] h-[36px] bg-no-repeat bg-contain text-[#A81600] text-center bg-[url('/main/read_more_bg.png')] ${roboto.className}`}
+              >
+                Read more
+              </p>
+            </div>
+            <div className="relative h-[136px] w-[236px] bg-no-repeat bg-[url('/main/ticket_thumb.png')] bg-contain"></div>
+          </div>
+
+          <div className="relative h-[150px] w-[375px] bg-no-repeat bg-[url('/main/ticket_bg.png')] bg-contain flex p-2">
+            <div className={`w-[100px] h-[150px] flex flex-col items-start justify-center`}>
+              <p className={`w-[100px] text-[#A81600] ${roboto.className}`}>Lead number one</p>
+              <p
+                className={`flex items-center justify-center w-[80px] h-[36px] bg-no-repeat bg-contain text-[#A81600] text-center bg-[url('/main/read_more_bg.png')] ${roboto.className}`}
+              >
+                Read more
+              </p>
+            </div>
+            <div className="relative h-[136px] w-[236px] bg-no-repeat bg-[url('/main/ticket_thumb.png')] bg-contain"></div>
+          </div>
+          <div className="relative h-[150px] w-[375px] bg-no-repeat bg-[url('/main/ticket_bg.png')] bg-contain flex p-2">
+            <div className={`w-[100px] h-[150px] flex flex-col items-start justify-center`}>
+              <p className={`w-[100px] text-[#A81600] ${roboto.className}`}>Lead number one</p>
+              <p
+                className={`flex items-center justify-center w-[80px] h-[36px] bg-no-repeat bg-contain text-[#A81600] text-center bg-[url('/main/read_more_bg.png')] ${roboto.className}`}
+              >
+                Read more
+              </p>
+            </div>
+            <div className="relative h-[136px] w-[236px] bg-no-repeat bg-[url('/main/ticket_thumb.png')] bg-contain"></div>
+          </div>
+
+          <div className="relative h-[150px] w-[375px] bg-no-repeat bg-[url('/main/ticket_bg.png')] bg-contain flex p-2">
+            <div className={`w-[100px] h-[150px] flex flex-col items-start justify-center`}>
+              <p className={`w-[100px] text-[#A81600] ${roboto.className}`}>Lead number one</p>
+              <p
+                className={`flex items-center justify-center w-[80px] h-[36px] bg-no-repeat bg-contain text-[#A81600] text-center bg-[url('/main/read_more_bg.png')] ${roboto.className}`}
+              >
+                Read more
+              </p>
+            </div>
+            <div className="relative h-[136px] w-[236px] bg-no-repeat bg-[url('/main/ticket_thumb.png')] bg-contain"></div>
+          </div>
+
+          <div className="relative h-[150px] w-[375px] bg-no-repeat bg-[url('/main/ticket_bg.png')] bg-contain flex p-2">
+            <div className={`w-[100px] h-[150px] flex flex-col items-start justify-center`}>
+              <p className={`w-[100px] text-[#A81600] ${roboto.className}`}>Lead number one</p>
+              <p
+                className={`flex items-center justify-center w-[80px] h-[36px] bg-no-repeat bg-contain text-[#A81600] text-center bg-[url('/main/read_more_bg.png')] ${roboto.className}`}
+              >
+                Read more
+              </p>
+            </div>
+            <div className="relative h-[136px] w-[236px] bg-no-repeat bg-[url('/main/ticket_thumb.png')] bg-contain"></div>
+          </div>
+
+          <div className="h-[100px]" />
 
           <div className="fixed z-50 w-full h-16 max-w-lg -translate-x-1/2 rounded-full bottom-[13px] left-1/2">
             <div className="flex flex-row justify-center">
