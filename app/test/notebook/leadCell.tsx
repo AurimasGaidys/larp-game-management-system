d14136d2afce43426da7870da75c78e27ecad81f
Aurimas Gaidys <aurimas.gaidys@gmail.com>
2023-12-15 18:40:41 +0200

    Update designs

--- app/test/notebook/leadCell.tsx
+++ app/test/notebook/leadCell.tsx
@@ -11,21 +11,21 @@ interface LeadCellProps {
 
 export const LeadCell = ({ lead }: LeadCellProps) => {
   return (
-    <div className="relative h-[205px] w-[390px] bg-no-repeat bg-[url('/kd/paper_bg.png')] bg-contain flex p-8 items-center justify-end">
-      <div
-        className={`w-[100px] h-[150px] flex flex-col items-start justify-center`}
-      >
-        <p className={`w-[100px] text-[#1A222F] ${handWriting.className}`}>
-          {lead}
-        </p>
-        <p
-          className={`flex items-center justify-center w-[80px] h-[36px] bg-no-repeat bg-contain text-[#A81600] text-center bg-[url('/kd/read_more_bg.png')] ${handWriting.className}`}
+    <div className="relative h-[205px] w-[390px] bg-no-repeat bg-contain flex p-[15px] items-center justify-end">
+      <div className="absolute h-[90px] w-[86px] bg-no-repeat bg-[url('/ghost.jpeg')] bg-cover bg-top -rotate-2 top-[26px] right-[71px]" />
+      <div className="absolute h-[205px] w-[390px] top-0 rigth-0 bg-no-repeat bg-[url('/kd/paper_bg.png')] bg-contain flex items-center justify-end">
+        <div
+          className={`absolute left-[60px] w-[100px] h-[150px] flex flex-col items-start justify-center -rotate-6`}
         >
-          Read more
-        </p>
-      </div>
-      <div className="relative h-[190px] w-[190px] bg-no-repeat bg-[url('/ghost.jpeg')] bg-[length:105px_105px] mt-[34px] bg-top">
-        <div className="relative h-[180px] w-[180px] bg-no-repeat bg-[url('/kd/photo_container_temp.png')] bg-contain -mt-[14px]" />
+          <p className={`w-[100px] text-[#1A222F] ${handWriting.className}`}>
+            {lead}
+          </p>
+          <p
+            className={`flex items-center justify-center w-[80px] h-[36px] bg-no-repeat bg-contain text-[#A81600] text-center bg-[url('/kd/read_more_bg.png')] ${handWriting.className}`}
+          >
+            Read more
+          </p>
+        </div>
       </div>
     </div>
   );
