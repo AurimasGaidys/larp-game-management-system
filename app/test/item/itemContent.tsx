384960499789f20477d28b498c65aa334b0c3ac6
Aurimas Gaidys <aurimas.gaidys@gmail.com>
2023-12-20 23:36:42 +0200

    BG paper added.

--- app/test/item/itemContent.tsx
+++ app/test/item/itemContent.tsx
@@ -11,15 +11,17 @@ const handWriting = Kalam({
 
 export const ItemContent = ({ name }: LeadListProps) => {
   return (
-    <div className="flex flex-col items-center">
-      <div className="h-[25px]" />
-      <div className="bg-bottom bg-no-repeat h-[79px] w-[371px] bg-[url('/kd/paper_content_top.png')] bg-contain"></div>
-      <div className="w-[370px] bg-[url('/kd/paper_content_middle.png')] bg-contain p-4">
-        <p className={`-mt-[50px] text-[#1A222F] ${handWriting.className} text-2xl`}>
+    <div className="flex flex-col items-center -mt-[44px]">
+      <div className="bg-bottom bg-no-repeat h-[79px] w-[371px] bg-[url('/kd/paper_content_top.png')] bg-contain z-10"></div>
+      <div className="absolute left-[48%] top-[230px] h-[704px] w-[519px] -translate-x-[50%]  bgh-center bg-no-repeat h-[389px] w-[582px] bg-[url('/kd/items/bg_paper.png')] bg-contain"></div>
+      <div className="w-[370px] bg-[url('/kd/paper_content_middle.png')] bg-contain p-4 z-10">
+        <p
+          className={`-mt-[50px] text-[#1A222F] ${handWriting.className} text-2xl`}
+        >
           {name}
         </p>
       </div>
-      <div className="bg-bottom bg-no-repeat h-[64px] w-[371px] bg-[url('/kd/paper_content_bottom.png')] bg-contain"></div>
+      <div className="bg-bottom bg-no-repeat h-[64px] w-[371px] bg-[url('/kd/paper_content_bottom.png')] bg-contain z-10"></div>
       <div className="h-[200px]" />
     </div>
   );
--- app/test/item/itemHeader.tsx
+++ app/test/item/itemHeader.tsx
@@ -22,8 +22,8 @@ export const ItemHeader = ({ name }: LeadListProps) => {
           </p>
         </div>
       </div>
-      <div className="relative -mt-[40px] h-[419px] w-[315px] bg-no-repeat bg-contain flex p-2 flex justify-center items-center z-5">
-      <div className="absolute h-[419px] w-[315px] bg-no-repeat bg-[url('/kd/bag.png')] bg-contain flex p-2 flex justify-center items-center z-5"></div>
+      <div className="relative -mt-[40px] h-[419px] w-[315px] bg-no-repeat bg-contain flex p-2 flex justify-center items-center z-30">
+      <div className="absolute h-[419px] w-[315px] bg-no-repeat bg-[url('/kd/bag.png')] bg-contain flex p-2 flex justify-center items-center z-30"></div>
         <div className=" h-[210px] w-[210px] bg-no-repeat bg-[url('/kd/magnifying_glass.png')] bg-contain flex z-1"></div>
       </div>
       <div className="h-[10px]" />
